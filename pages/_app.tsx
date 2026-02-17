3ddd609b40e0cdb2c0597fef19312c5829fa8c8e
Nursuni <202450493@jbnu.ac.kr>
2026-02-17 18:58:29 +0900

    fix: setup intial folders

---
+++ pages/_app.tsx
@@ -0,0 +1,20 @@
+import type { AppProps } from 'next/app';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import React, { useState } from 'react';
+import { light } from '../scss/MaterialTheme';
+
+import { appWithTranslation } from 'next-i18next';
+
+const App = ({ Component, pageProps }: AppProps) => {
+	// @ts-ignore
+	const [theme, setTheme] = useState(createTheme(light));
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<Component {...pageProps} />
+		</ThemeProvider>
+	);
+};
+
+export default appWithTranslation(App);
---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+	return (
+		<Html lang="en">
+			<Head>
+				<meta name="robots" content="index,follow" />
+				<link rel="icon" type="image/png" href="/img/logo/favicon.svg" />
+
+				{/* SEO */}
+				<meta
+					name="keywords"
+					content="Glowly, cosmetics, skincare, makeup, beauty products, korean beauty, K-beauty, online cosmetics store"
+				/>
+				<meta
+					name="description"
+					content={
+						'Discover premium skincare and makeup products for glowing, healthy skin. Shop the latest beauty trends at Glowly | ' +
+						'Откройте для себя премиальную косметику и средства по уходу за кожей для сияющей и здоровой кожи. Покупайте лучшие бьюти-продукты на Glowly | ' +
+						'Glowly에서 프리미엄 스킨케어와 메이크업 제품을 만나보세요. 건강하고 빛나는 피부를 위한 최고의 뷰티 제품을 쇼핑하세요.'
+					}
+				/>
+
+				{/* Open Graph (Important for social sharing) */}
+				<meta property="og:title" content="Glowly – Premium Cosmetics & Skincare" />
+				<meta property="og:description" content="Shop high-quality skincare and makeup products for radiant beauty." />
+				<meta property="og:type" content="website" />
+				<meta property="og:image" content="/img/logo/logoWhite.svg" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+}
