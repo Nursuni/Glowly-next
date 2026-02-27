9abf8544fa213e4225eef0c6649fb17dc356bb48
Nursuni <202450493@jbnu.ac.kr>
2026-02-28 02:59:05 +0900

    feat: develop Admin GraphQL query and mutation

---
+++ apollo/admin/mutation.ts
@@ -0,0 +1,155 @@
+import { gql } from '@apollo/client';
+
+/**************************
+ *         MEMBER         *
+ *************************/
+
+export const UPDATE_MEMBER_BY_ADMIN = gql`
+	mutation UpdateMemberByAdmin($input: MemberUpdate!) {
+		updateMemberByAdmin(input: $input) {
+			_id
+			memberStatus
+
+			memberPhone
+			memberNick
+			memberFullName
+			memberImage
+			memberAddress
+			memberDesc
+			memberProducts
+
+			memberRank
+			memberArticles
+			memberPoints
+			memberLikes
+			memberViews
+			memberWarnings
+			memberBlocks
+			deletedAt
+			createdAt
+			updatedAt
+			accessToken
+		}
+	}
+`;
+
+/**************************
+ *        PRODUCT        *
+ *************************/
+
+export const UPDATE_PRODUCT_BY_ADMIN = gql`
+	mutation UpdateProductByAdmin($input: ProductUpdate!) {
+		updateProductByAdmin(input: $input) {
+			_id
+			productType
+			productStatus
+			productTitle
+			productPrice
+			discountType
+			discountValue
+			volume
+			volumeUnit
+			skinType
+			productTarget
+			ingredientType
+			stock
+			productViews
+			productLikes
+			productComments
+			productImages
+			productDesc
+			memberId
+			deletedAt
+			createdAt
+			updatedAt
+		}
+	}
+`;
+
+export const REMOVE_PRODUCT_BY_ADMIN = gql`
+	mutation RemoveProductByAdmin($input: String!) {
+		removeProductByAdmin(productId: $input) {
+			_id
+			productType
+			productStatus
+			productTitle
+			productPrice
+			discountType
+			discountValue
+			volume
+			volumeUnit
+			skinType
+			productTarget
+			ingredientType
+			stock
+			productViews
+			productLikes
+			productComments
+			productImages
+			productDesc
+			memberId
+			deletedAt
+			createdAt
+			updatedAt
+		}
+	}
+`;
+
+/**************************
+ *      BOARD-ARTICLE     *
+ *************************/
+
+export const UPDATE_BOARD_ARTICLE_BY_ADMIN = gql`
+	mutation UpdateBoardArticleByAdmin($input: BoardArticleUpdate!) {
+		updateBoardArticleByAdmin(input: $input) {
+			_id
+			articleCategory
+			articleStatus
+			articleTitle
+			articleContent
+			articleImage
+			articleViews
+			articleLikes
+			memberId
+			createdAt
+			updatedAt
+		}
+	}
+`;
+
+export const REMOVE_BOARD_ARTICLE_BY_ADMIN = gql`
+	mutation RemoveBoardArticleByAdmin($input: String!) {
+		removeBoardArticleByAdmin(articleId: $input) {
+			_id
+			articleCategory
+			articleStatus
+			articleTitle
+			articleContent
+			articleImage
+			articleViews
+			articleLikes
+			memberId
+			createdAt
+			updatedAt
+		}
+	}
+`;
+
+/**************************
+ *         COMMENT        *
+ *************************/
+
+export const REMOVE_COMMENT_BY_ADMIN = gql`
+	mutation RemoveCommentByAdmin($input: String!) {
+		removeCommentByAdmin(commentId: $input) {
+			_id
+			commentStatus
+			commentGroup
+			commentContent
+			commentRefId
+			memberId
+			createdAt
+			updatedAt
+		}
+	}
+`;
---
+++ apollo/admin/query.ts
@@ -0,0 +1,200 @@
+import { gql } from '@apollo/client';
+
+/**************************
+ *         MEMBER         *
+ *************************/
+
+export const GET_ALL_MEMBERS_BY_ADMIN = gql`
+	query GetAllMembersByAdmin($input: MembersInquiry!) {
+		getAllMembersByAdmin(input: $input) {
+			list {
+				_id
+				memberType
+				memberStatus
+				memberAuthType
+				memberPhone
+				memberNick
+				memberFullName
+				memberImage
+				memberAddress
+				memberDesc
+				memberWarnings
+				memberGender
+				memberBlocks
+				memberProducts
+				memberRank
+				memberArticles
+				memberPoints
+				memberLikes
+				memberViews
+				deletedAt
+				createdAt
+				updatedAt
+				accessToken
+			}
+			metaCounter {
+				total
+			}
+		}
+	}
+`;
+
+/**************************
+ *        PRODUCT        *
+ *************************/
+
+export const GET_ALL_PRODUCTS_BY_ADMIN = gql`
+	query GetAllProductsByAdmin($input: AllProductsInquiry!) {
+		GetAllProductsByAdmin(input: $input) {
+			list {
+				_id
+				productType
+				productStatus
+				productTitle
+				productPrice
+				discountType
+				discountValue
+				volume
+				volumeUnit
+				skinType
+				productTarget
+				ingredientType
+				stock
+				productViews
+				productLikes
+				productComments
+				productImages
+				productDesc
+				memberId
+				deletedAt
+				createdAt
+				updatedAt
+				memberData {
+					_id
+					memberType
+					memberStatus
+					memberAuthType
+					memberPhone
+					memberNick
+					memberFullName
+					memberImage
+					memberAddress
+					memberDesc
+					memberWarnings
+					memberBlocks
+					memberProducts
+					memberGender
+					memberRank
+					memberPoints
+					memberLikes
+					memberViews
+					deletedAt
+					createdAt
+					updatedAt
+					accessToken
+				}
+			}
+			metaCounter {
+				total
+			}
+		}
+	}
+`;
+
+/**************************
+ *      BOARD-ARTICLE     *
+ *************************/
+
+export const GET_ALL_BOARD_ARTICLES_BY_ADMIN = gql`
+	query GetAllBoardArticlesByAdmin($input: AllBoardArticlesInquiry!) {
+		getAllBoardArticlesByAdmin(input: $input) {
+			list {
+				_id
+				articleCategory
+				articleStatus
+				articleTitle
+				articleContent
+				articleImage
+				articleViews
+				articleLikes
+				memberId
+				createdAt
+				updatedAt
+				memberData {
+					_id
+					memberType
+					memberStatus
+					memberAuthType
+					memberPhone
+					memberNick
+					memberFullName
+					memberImage
+					memberAddress
+					memberDesc
+					memberWarnings
+					memberBlocks
+					memberProducts
+					memberGender
+					memberRank
+					memberPoints
+					memberLikes
+					memberViews
+					deletedAt
+					createdAt
+					updatedAt
+					accessToken
+				}
+			}
+			metaCounter {
+				total
+			}
+		}
+	}
+`;
+
+/**************************
+ *         COMMENT        *
+ *************************/
+
+export const GET_COMMENTS = gql`
+	query GetComments($input: CommentsInquiry!) {
+		getComments(input: $input) {
+			list {
+				_id
+				commentStatus
+				commentGroup
+				commentContent
+				commentRefId
+				memberId
+				createdAt
+				updatedAt
+				memberData {
+					_id
+					memberType
+					memberStatus
+					memberAuthType
+					memberPhone
+					memberNick
+					memberFullName
+					memberImage
+					memberAddress
+					memberDesc
+					memberWarnings
+					memberBlocks
+					memberProducts
+					memberRank
+					memberPoints
+					memberLikes
+					memberViews
+					deletedAt
+					createdAt
+					updatedAt
+					accessToken
+				}
+			}
+			metaCounter {
+				total
+			}
+		}
+	}
+`;
